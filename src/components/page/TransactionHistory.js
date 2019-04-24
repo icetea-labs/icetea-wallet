c7d18a710dafda0261e9f8bde6f5821697f5f0cb
hoanghuy226 <hoanghuy226@gmail.com>
2019-04-24 22:14:24 +0700

    add pagintion

--- src/components/page/TransactionHistory.js
+++ src/components/page/TransactionHistory.js
@@ -1,7 +1,130 @@
 import React, { Component } from 'react';
-import './TransactionHistory.css'
+import { Link } from 'react-router-dom';
+import './TransactionHistory.css';
+import { decodeTX } from 'icetea-web3/src/utils';
+import { ecc } from 'icetea-common';
+import tweb3 from '../../service/tweb3';
+import PuViewTx from './poup/PuViewTx';
+import Select from 'rc-select';
+import Pagination from 'rc-pagination';
+import localeInfo from 'rc-pagination/lib/locale/en_US';
+import './../../assets/styles/pagination.css'
+import './../../assets/styles/locale.css'
 
 class TransactionHistory extends Component {
+  constructor() {
+    super();
+    this.state = {
+      transactions: [],
+      txToTable: [],
+      showDetailTx: false,
+      current: 1,
+      pageSize: 10
+    }
+  }
+
+  componentDidMount() {
+    this.renderTransactions();
+  }
+  fmtTxs = (txs) => {
+    Object.keys(txs).forEach(k => {
+      const t = txs[k]
+      t.shash = t.hash//this.fmtHex(t.hash)
+      t.blockHeight = t.height
+
+      const data = decodeTX(t.tx)
+      let from = data.from
+      if (!from) {
+        const pubkey = data.evidence.pubkey || data.evidence[0].pubkey
+        from = ecc.toAddress(pubkey)
+      }
+      t.from = from//this.fmtHex(from, 6)
+      t.to = data.to//this.fmtHex(data.to, 6)
+      t.value = data.value
+      t.fee = data.fee
+
+      t.status = t.tx_result.code ? 'Error' : 'Success'
+
+      t.txType = 'transfer'
+      data.data = JSON.parse(data.data) || {}
+      if (data.data.op === 0) {
+        t.txType = 'deploy'
+        // t.to = fmtHex(t.tx_result.data);
+      } else if (data.data.op === 1) {
+        t.txType = 'call'
+      }
+    })
+    return txs.reverse()
+  }
+  // fmtHex = (hex, c) => {
+  //   if (!hex || hex.length < c * 2 + 4) return hex
+  //   if (hex.indexOf('.') >= 0) return hex
+  //   c = c || 4
+  //   return hex.substr(0, c) + '…' + hex.substr(-c)
+  // }
+  goDetailHash = (hash) => {
+    console.log(hash)
+  }
+  viewDetail = (hash) => {
+    console.log('-viewDetail-', hash)
+    this.setState({ showDetailTx: true, hashValue: hash });
+  }
+  closeViewDetail() {
+    this.setState({
+      showDetailTx: !this.state.showDetailTx
+    });
+  }
+  renderTransactions = async (current=this.state.current, pageSize=this.state.pageSize) => {
+    try {
+      var myTxs = await tweb3.searchTransactions('tx.height>0');
+      var transactions = this.fmtTxs(myTxs.txs);
+      var form = (current - 1) * pageSize;
+      var to = form + pageSize;
+      if(to > transactions.length) to = transactions.length;
+      transactions = transactions.slice(form, to)
+      // this will re render the view with new data
+      this.setState({
+        transactions: transactions,
+        txToTable: transactions.map((tx, index) => (
+          <tr key={index}>
+            <td><div className="sc-gojNiO jQgIyo" onClick={() => this.goDetailHash(tx.hash)}>{tx.hash}</div></td>
+            <td><div>{tx.txType}</div></td>
+            <td><div>{tx.status}</div></td>
+            <td><div>{tx.txType}</div></td>
+            <td><div>{tx.value}</div></td>
+            <td><div>{tx.fee}</div></td>
+            <td><div><i className="fa fa-ellipsis-h" onClick={() => this.viewDetail(tx.hash)} ></i></div></td>
+          </tr>
+        ))
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  
+  getTxByHash = (hash) => {
+    var tx = this.state.transactions.filter(el => {
+      return el.shash === hash;
+    })
+    return tx[0];
+  }
+  
+  onShowSizeChange=(current, pageSize)=> {
+    // console.log(current);
+    // console.log(pageSize);
+    this.renderTransactions(current, pageSize);
+    this.setState({
+      pageSize: pageSize,
+      current: current
+    });
+  }
+  onChange = (page) => {
+    // console.log(page);
+    this.renderTransactions(page);
+    this.setState({
+      current: page,
+    });
+  }
   render() {
     return (
       <div className="sc-cZLAbK gUtTAS">
@@ -20,23 +143,35 @@ class TransactionHistory extends Component {
                       <div className="sc-esjQYD fchNtk">Date<span className="sc-kfGgVZ hfdPRR"><i
                         className="iconfont icon-sort_desc sc-dnqmqq iiYHFz" size="16" color=""></i></span></div>
                     </th>
-                    <th>Pair</th>
+                    <th>Status</th>
                     <th>Type</th>
                     <th>Value</th>
                     <th>TxFee</th>
                     <th></th>
                   </tr>
                 </thead>
-                <tbody className="sc-hwwEjo jWardX"></tbody>
+                <tbody className="sc-hwwEjo jWardX">
+                  {this.state.txToTable}
+                </tbody>
               </table>
             </div>
           </div>
-        </div>
-        <div className="sc-kXeGPI hpRMTi">
-          <div className="sc-iVOTot kOwkAM">Transaction History</div>
-          <div>
-            <div className="infinite-scroll-component "></div>
+          <div className="sc-jDwBTQ leNfoz">
+            <div className="sc-hMqMXs djyRfV">
+            <Pagination
+              selectComponentClass={Select}
+              showQuickJumper
+              showSizeChanger
+              defaultPageSize={this.state.pageSize}
+              defaultCurrent={this.state.current}
+              onShowSizeChange={this.onShowSizeChange}
+              onChange={this.onChange}
+              total={this.state.transactions.length}
+              locale={localeInfo}
+            />
+            </div>
           </div>
+          {this.state.showDetailTx ? <PuViewTx tx={this.getTxByHash(this.state.hashValue)} closeViewDetail={this.closeViewDetail.bind(this)} /> : ''}
         </div>
       </div>
     );
