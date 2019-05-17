ffdd5ee49c7cf7331bf67f4ef5af0b66ec1a3b4b
Sotatek-TinDuong <tin.duong@sotatek.com>
2019-05-17 14:24:57 +0700

    fix class tooltip

--- src/components/pages/Dapp/BotStore.js
+++ src/components/pages/Dapp/BotStore.js
@@ -66,7 +66,7 @@ const BotItems = styled.div`
       overflow: hidden;
     }
     &:hover {
-      .sc-qrIAp{
+      .sc-iGrrsa{
         visibility: visible;
         opacity: 1;
         transform: translateY(0);
