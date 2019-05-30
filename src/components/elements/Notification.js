582f99e82424ac0da9bd4a6ce2bdcb1726fb6a51
LuongHV <softkiss243@gmail.com>
2019-05-30 19:51:07 +0700

    notiWarning

--- src/components/elements/Notification.js
+++ src/components/elements/Notification.js
@@ -2,12 +2,13 @@ import React from 'react';
 import 'rc-notification/assets/index.css';
 import Notification from 'rc-notification';
 import iconSuccess from '../../assets/img/success-icon.png';
+import iconWarning from '../../assets/img/warning-icon.png';
 import { checkDevice } from './utils';
 
 let notification = null;
 
 const alertCus = function(e) {
-  var alert = document.createElement('div');
+  let alert = document.createElement('div');
   alert.className = 'alert';
   alert.innerText = e;
   document.body.appendChild(alert);
@@ -40,7 +41,7 @@ const notifi = {
           duration: '5',
           content: (
             <span className="notification">
-              <img src={iconSuccess} alt="" />
+              <img src={iconWarning} alt="" />
               {e}
             </span>
           ),
