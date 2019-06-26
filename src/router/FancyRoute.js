d98d7a24f87a212c57012b3ee67ffe3cbe7dc170
LuongHV <softkiss243@gmail.com>
2019-06-26 16:06:08 +0700

    fixHomeCardStyle

--- src/router/FancyRoute.js
+++ src/router/FancyRoute.js
@@ -10,9 +10,9 @@ class FancyRoute extends React.Component {
     // nprogress.set(0.0); // To set a progress percentage, call .set(n), where n is a number between 0..1
     nprogress.inc(0.5); // To increment the progress bar, just use .inc(). This increments it with a random amount. This will never get to 100%: use it for every image load (or similar).If you want to increment by a specific value, you can pass that as a parameter
     nprogress.configure({ ease: 'ease', speed: 500 }); // Adjust animation settings using easing (a CSS easing string) and speed (in ms). (default: ease and 200)
-    nprogress.configure({ trickleRate: 0.1, trickleSpeed: 800 }); //Adjust how often to trickle/increment, in ms.
-    nprogress.configure({ showSpinner: false }); //Turn off loading spinner by setting it to false. (default: true)
-    nprogress.configure({ parent: 'body' }); //specify this to change the parent container. (default: body)
+    nprogress.configure({ trickleRate: 0.1, trickleSpeed: 800 }); // Adjust how often to trickle/increment, in ms.
+    nprogress.configure({ showSpinner: false }); // Turn off loading spinner by setting it to false. (default: true)
+    nprogress.configure({ parent: 'body' }); // specify this to change the parent container. (default: body)
   }
 
   componentDidMount() {
